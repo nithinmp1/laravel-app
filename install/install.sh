c112f91276234af97719c429a70f39d513b76c95
nithinmp1 <42617829+nithinmp1@users.noreply.github.com>
2024-04-08 10:25:41 +0530

    Update install.sh
    installation changes added

--- install/install.sh
+++ install/install.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+# Function for error handling
+handle_error() {
+    echo "Error: $1" >&2
+    exit 1
+}
+
+# Move contents of env.txt to .env
+echo "Moving contents of env.txt to .env..."
+cat env.txt > .env || handle_error "Failed to move contents of env.txt to .env"
+
+composer update
+
+php artisan key:generate
+
 # Set up Docker containers
-docker-compose up -d
+docker-compose up -d || handle_error "Failed to start Docker containers"
 
 # Wait for containers to start
-sleep 10
+sleep_time=10
+echo "Waiting for containers to start..."
+sleep $sleep_time
+
+# # Update Laravel dependencies
+# echo "Updating Laravel dependencies..."
+# docker-compose exec -T laravel_app composer update || handle_error "Composer update failed"
 
 # Run database migrations
-docker-compose exec -T laravel_app_1 php artisan migrate
+echo "Running database migrations..."
+docker-compose exec -T laravel_app php artisan migrate || handle_error "Database migration failed"
+
+echo "Database migration completed successfully."
